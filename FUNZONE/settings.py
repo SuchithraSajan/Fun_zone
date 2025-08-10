08e12ed6e0fe7fcb26603c4eafd67163254d21d8
SuchithraSajan <kukkusuchithra010@gmail.com>
2025-08-10 10:50:40 +0530

    z

--- FUNZONE/settings.py
+++ FUNZONE/settings.py
@@ -11,15 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
 import os
+from . import dj_database_url
 
 
-import dj_database_url
-
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,7 +86,9 @@ DATABASES = {
     }
 }
 
-
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
